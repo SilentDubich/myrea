6f86df5ce82916a3948013ec21f8d94d881a78d5
SilentDubich <kirill.dubov.2012@mail.ru>
2020-03-27 16:55:53 +0300

    Updates

---
+++ precache-manifest.64e9d47a134c12b3cdd6d402774e4ae2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f35315769086c84f01b255e34d700df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8faddfbdc24895c713af",
+    "url": "/static/css/main.bc15605f.chunk.css"
+  },
+  {
+    "revision": "73c70a267e070a92f92c",
+    "url": "/static/js/2.9b4d89d1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.9b4d89d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8faddfbdc24895c713af",
+    "url": "/static/js/main.e784ebd5.chunk.js"
+  },
+  {
+    "revision": "0f91f93175e0673870be",
+    "url": "/static/js/runtime-main.4b22a48f.js"
+  },
+  {
+    "revision": "33209dd6cd627aa1036e3f57ab0fd6bc",
+    "url": "/static/media/Agronom.33209dd6.jpg"
+  },
+  {
+    "revision": "337f9a9dedae9467b7a6642b35590306",
+    "url": "/static/media/Spinner-1.4s-3a356a.337f9a9d.svg"
+  },
+  {
+    "revision": "56b5ac7d546d2bb54162f9ae1c667b51",
+    "url": "/static/media/ali.56b5ac7d.png"
+  },
+  {
+    "revision": "587efa6ed2108ffe93f7fefbb2ed4039",
+    "url": "/static/media/cat.587efa6e.jpeg"
+  },
+  {
+    "revision": "fc29e3e3f2e783e1bbe8c95a5eadec57",
+    "url": "/static/media/fedor.fc29e3e3.jpg"
+  },
+  {
+    "revision": "ab094aac280dec19ad24d4aa63a564c5",
+    "url": "/static/media/goliy.ab094aac.jpg"
+  },
+  {
+    "revision": "4235ff499b352fbd5a59975090d65b6c",
+    "url": "/static/media/mordovia.4235ff49.jpg"
+  },
+  {
+    "revision": "e8c8c61aa5c979f476e3ac40b79a4c3a",
+    "url": "/static/media/nullPhoto.e8c8c61a.jpg"
+  },
+  {
+    "revision": "698b2495d0c6e619f550e2989ff8ffcf",
+    "url": "/static/media/pendalf.698b2495.jpg"
+  },
+  {
+    "revision": "1cb38a93d2033edafbd682ffe9af6dc2",
+    "url": "/static/media/sarumyan.1cb38a93.jpg"
+  },
+  {
+    "revision": "5a557dc002bbfe7bd087e19e5580af2e",
+    "url": "/static/media/senya.5a557dc0.jpg"
+  }
+]);
